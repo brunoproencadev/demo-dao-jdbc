27b4681ab2dd5e2181f1b22a69a73647ab66170b
Bruno Proença Silva <brunoproencasilva@gmail.com>
2026-05-01 20:36:51 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department department = new Department(1, "Books");
+		Seller seller = new Seller(21, "Bob Brown", "bob@gmail.com", new Date(), 3000.0, department);
+		
 		System.out.println(department);
+		System.out.println(seller);
 
 	}
 
