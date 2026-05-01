bdbbf02fc9e436be927f9bbf17796e5089cdaac5
Bruno Proença Silva <brunoproencasilva@gmail.com>
2026-05-01 20:46:31 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entites.Department;
 import model.entites.Seller;
 
@@ -11,6 +13,7 @@ public class Program {
 		
 		Department department = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob Brown", "bob@gmail.com", new Date(), 3000.0, department);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(department);
 		System.out.println(seller);
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,37 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao {
+	
+	@Override
+	public void insert(model.entites.Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(model.entites.Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public model.entites.Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public java.util.List<model.entites.Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
